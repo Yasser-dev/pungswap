95387d76d5383254a82b44b3ee2d817a3467b943
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-05-17 22:44:49 +0800

    React tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  render() {
+    return <div className="text-blue-400"> PungSwap</div>;
+  }
+}
+export default App;
